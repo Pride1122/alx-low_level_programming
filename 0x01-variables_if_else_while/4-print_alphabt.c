842ce5a23dbff6f25d9c08b30b352057abc32c4c
pride1122 <prideculture0@gmail.com>
2023-06-17 03:35:17 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 /**
-* main - prints the alphabet in lowercase,
-* followed by a new line
-* Return: Always 0 (Success)
+*main - prints the alphabet in lowercase,
+*followed by a new line
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-char ch'a';
-while (ch <= 'z')
-{
-if (ch != 'e' && ch != 'q')
-{
+char ch;
+for(ch = 'a'; ch <= 'z'; ch++);
+{       
 putchar(ch);
-}
-ch++;
-}
+}       
 putchar('\n');
 return (0);
 }
