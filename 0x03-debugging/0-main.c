736fafacc7da4f646c7603d55883b034016d4cae
pride1122 <prideculture0@gmail.com>
2023-06-22 03:37:13 -0700

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
-* main - absolute Test function for positive or negative
+* main - The test function for positive or negative
 * Return: 0
 */
+
 int main(void)
+
 {
-int z;
-z = 0;
-positive_or_negative(z);
+
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
